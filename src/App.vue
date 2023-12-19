6ed20346c147c862e6c28fef50f62920ee12a06a
wang_cheng <wang_cheng@cestc.cn>
2023-12-19 19:44:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Person />
+</template>
+
+<script lang="ts">
+  import Person from "./components/Person.vue";
+
+  export default {
+    name: "App", // 组件名
+    components: { Person }, // 注册组件
+  };
+</script>
+
+<style>
+  .app {
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="showTel">查看联系方式</button>
+  </div>
+</template>
+
+<!-- <script lang="ts">
+  export default {
+    name: "Person",
+  }
+</script> -->
+
+<script lang="ts" setup name="Person234">
+  let name = "张三";
+  let age = 18;
+  let tel = "13888888888";
+
+  function changeName() {
+    name = "zhang-san";
+    console.log(name);
+  }
+  function changeAge() {
+    age += 1;
+    console.log(age);
+  }
+  function showTel() {
+    alert(tel);
+  }
+</script>
+
+<style scoped>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+
+  button {
+    margin: 0 5px;
+  }
+</style>
